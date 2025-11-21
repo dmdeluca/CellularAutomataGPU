8ffb1260261b96a15233767e5e86de30d964e6eb
David DeLuca <dvdmdlc@gmail.com>
2025-11-21 18:43:06 -0500

    Initial Commit

---
+++ CellularAutomataProject2/CellularAutomataProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  CellularAutomataProject2App.swift
+//  CellularAutomataProject2
+//
+//  Created by David DeLuca on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CellularAutomataProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
